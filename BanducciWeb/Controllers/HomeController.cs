e5cf7d91936d2fd89ecde60975729e960ceed184
Matt Cook <Cooker8200@hotmail.com>
2017-07-26 21:48:56 -0500

    ntier completed.  add management functions as needed.

--- BanducciWeb/Controllers/HomeController.cs
+++ BanducciWeb/Controllers/HomeController.cs
@@ -34,6 +34,43 @@ namespace BanducciWeb.Controllers
             return PartialView("_Partnerships");
         }
 
+        public ActionResult _JonB_GetBio()
+        {
+            return PartialView("_JonBanducciBio");
+        }
+
+        public ActionResult _MichelleB_GetBio()
+        {
+            return PartialView("_MichelleBanducciBio");
+        }
+
+        public ActionResult _Therese_GetBio()
+        {
+            return PartialView("_ThereseWunderlichBio");
+        }
+        public ActionResult _Jim_GetBio()
+        {
+            return PartialView("_JimFuegnerBio");
+        }
+        public ActionResult _Angie_GetBio()
+        {
+            return PartialView("_AngieBuscherBio");
+        }
+
+        public ActionResult _Julie_GetBio()
+        {
+            return PartialView("_JulieBio");
+        }
+
+        public ActionResult _Barb_GetBio()
+        {
+            return PartialView("_BarbBio");
+        }
+
+        public ActionResult _Carol_GetBio()
+        {
+            return PartialView("_CarolBio");
+        }
         public ActionResult CurrentNews()
         {
             return View();
---
+++ BanducciWeb/Scripts/ProfileScripts.js
@@ -0,0 +1,77 @@
+﻿console.log("Profile Scripts running");
+
+//leadership profile ajax calls
+$(document).ready(function () {
+    $("#jonb_bio").click(function () {
+        $.ajax({
+            url: "/home/_JonB_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#michelleb_bio").click(function () {
+        $.ajax({
+            url: "/home/_MichelleB_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#therese_bio").click(function () {
+        $.ajax({
+            url: "/home/_Therese_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#jim_bio").click(function () {
+        $.ajax({
+            url: "/home/_Jim_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#angie_bio").click(function () {
+        $.ajax({
+            url: "/home/_Angie_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#julie_bio").click(function () {
+        $.ajax({
+            url: "/home/_Julie_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#barb_bio").click(function () {
+        $.ajax({
+            url: "/home/_Barb_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+    $("#carol_bio").click(function () {
+        $.ajax({
+            url: "/home/_carol_GetBio",
+            success: function (result) {
+                $("#bio_area").html(result);
+            }
+        })
+    })
+
+})
